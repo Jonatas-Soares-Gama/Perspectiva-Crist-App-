8c4637ebff2f1d74272486fc79a4aace349f37cd
Jonatas-Soares-Gama <jonatassoaresdagama@gmail.com>
2023-03-07 23:07:26 -0300

    first commit

---
+++ perspectiva Cristã/Extension-Utilities/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  perspectiva Cristã
+//
+//  Created by Jonatas Gama on 06/03/23.
+//
+
+import Foundation
---
+++ perspectiva Cristã/Sources/Controller/FavoritController.swift
@@ -0,0 +1,29 @@
+//
+//  FavoritController.swift
+//  perspectiva Cristã
+//
+//  Created by Jonatas Gama on 07/03/23.
+//
+
+import UIKit
+
+class FavoritController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
